88d349509646c62aa72fd6cad6f2cccfaadbf53e
ViralEnactOn <vsjariwala@enacton.com>
2023-09-01 17:20:20 +0530

    change in respone util

--- config/responseUtil.js
+++ config/responseUtil.js
@@ -1,5 +1,5 @@
-function sendResponse(res, statusCode, message, data = null) {
-  res.status(statusCode).send(data);
+function sendResponse(res, data = null) {
+  res.send(data);
 }
 
 module.exports = sendResponse;
